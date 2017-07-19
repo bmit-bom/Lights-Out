69f92d488baf21006eb74308a872d406655ab606
marko.boltizar <marko.boltizar@ESPAS.CH>
2017-07-19 09:49:14 +0200

    useless comment + icon

---
+++ lights out/Form1.Designer.cs
@@ -0,0 +1,406 @@
+﻿namespace lights_out
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.A1 = new System.Windows.Forms.Button();
+            this.A2 = new System.Windows.Forms.Button();
+            this.A3 = new System.Windows.Forms.Button();
+            this.A4 = new System.Windows.Forms.Button();
+            this.A5 = new System.Windows.Forms.Button();
+            this.Reset = new System.Windows.Forms.Button();
+            this.B1 = new System.Windows.Forms.Button();
+            this.B2 = new System.Windows.Forms.Button();
+            this.B3 = new System.Windows.Forms.Button();
+            this.B4 = new System.Windows.Forms.Button();
+            this.B5 = new System.Windows.Forms.Button();
+            this.C1 = new System.Windows.Forms.Button();
+            this.C2 = new System.Windows.Forms.Button();
+            this.C3 = new System.Windows.Forms.Button();
+            this.C4 = new System.Windows.Forms.Button();
+            this.C5 = new System.Windows.Forms.Button();
+            this.D1 = new System.Windows.Forms.Button();
+            this.D2 = new System.Windows.Forms.Button();
+            this.D3 = new System.Windows.Forms.Button();
+            this.D4 = new System.Windows.Forms.Button();
+            this.D5 = new System.Windows.Forms.Button();
+            this.E1 = new System.Windows.Forms.Button();
+            this.E2 = new System.Windows.Forms.Button();
+            this.E3 = new System.Windows.Forms.Button();
+            this.E4 = new System.Windows.Forms.Button();
+            this.E5 = new System.Windows.Forms.Button();
+            this.PatternSetup = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // A1
+            // 
+            this.A1.BackColor = System.Drawing.Color.Black;
+            this.A1.Location = new System.Drawing.Point(13, 13);
+            this.A1.Name = "A1";
+            this.A1.Size = new System.Drawing.Size(50, 50);
+            this.A1.TabIndex = 0;
+            this.A1.UseVisualStyleBackColor = false;
+            this.A1.Click += new System.EventHandler(this.A1_Click);
+            // 
+            // A2
+            // 
+            this.A2.BackColor = System.Drawing.Color.Black;
+            this.A2.Location = new System.Drawing.Point(69, 13);
+            this.A2.Name = "A2";
+            this.A2.Size = new System.Drawing.Size(50, 50);
+            this.A2.TabIndex = 0;
+            this.A2.UseVisualStyleBackColor = false;
+            this.A2.Click += new System.EventHandler(this.A2_Click);
+            // 
+            // A3
+            // 
+            this.A3.BackColor = System.Drawing.Color.Black;
+            this.A3.Location = new System.Drawing.Point(125, 13);
+            this.A3.Name = "A3";
+            this.A3.Size = new System.Drawing.Size(50, 50);
+            this.A3.TabIndex = 0;
+            this.A3.UseVisualStyleBackColor = false;
+            this.A3.Click += new System.EventHandler(this.A3_Click);
+            // 
+            // A4
+            // 
+            this.A4.BackColor = System.Drawing.Color.Black;
+            this.A4.Location = new System.Drawing.Point(181, 13);
+            this.A4.Name = "A4";
+            this.A4.Size = new System.Drawing.Size(50, 50);
+            this.A4.TabIndex = 0;
+            this.A4.UseVisualStyleBackColor = false;
+            this.A4.Click += new System.EventHandler(this.A4_Click);
+            // 
+            // A5
+            // 
+            this.A5.BackColor = System.Drawing.Color.Black;
+            this.A5.Location = new System.Drawing.Point(237, 13);
+            this.A5.Name = "A5";
+            this.A5.Size = new System.Drawing.Size(50, 50);
+            this.A5.TabIndex = 0;
+            this.A5.UseVisualStyleBackColor = false;
+            this.A5.Click += new System.EventHandler(this.A5_Click);
+            // 
+            // Reset
+            // 
+            this.Reset.Location = new System.Drawing.Point(125, 299);
+            this.Reset.Name = "Reset";
+            this.Reset.Size = new System.Drawing.Size(162, 50);
+            this.Reset.TabIndex = 0;
+            this.Reset.Text = "Reset";
+            this.Reset.UseVisualStyleBackColor = true;
+            this.Reset.Click += new System.EventHandler(this.Reset_Click);
+            // 
+            // B1
+            // 
+            this.B1.BackColor = System.Drawing.Color.Black;
+            this.B1.Location = new System.Drawing.Point(13, 69);
+            this.B1.Name = "B1";
+            this.B1.Size = new System.Drawing.Size(50, 50);
+            this.B1.TabIndex = 1;
+            this.B1.UseVisualStyleBackColor = false;
+            this.B1.Click += new System.EventHandler(this.B1_Click);
+            // 
+            // B2
+            // 
+            this.B2.BackColor = System.Drawing.Color.Black;
+            this.B2.Location = new System.Drawing.Point(69, 69);
+            this.B2.Name = "B2";
+            this.B2.Size = new System.Drawing.Size(50, 50);
+            this.B2.TabIndex = 2;
+            this.B2.UseVisualStyleBackColor = false;
+            this.B2.Click += new System.EventHandler(this.B2_Click);
+            // 
+            // B3
+            // 
+            this.B3.BackColor = System.Drawing.Color.Black;
+            this.B3.Location = new System.Drawing.Point(125, 69);
+            this.B3.Name = "B3";
+            this.B3.Size = new System.Drawing.Size(50, 50);
+            this.B3.TabIndex = 3;
+            this.B3.UseVisualStyleBackColor = false;
+            this.B3.Click += new System.EventHandler(this.B3_Click);
+            // 
+            // B4
+            // 
+            this.B4.BackColor = System.Drawing.Color.Black;
+            this.B4.Location = new System.Drawing.Point(181, 69);
+            this.B4.Name = "B4";
+            this.B4.Size = new System.Drawing.Size(50, 50);
+            this.B4.TabIndex = 4;
+            this.B4.UseVisualStyleBackColor = false;
+            this.B4.Click += new System.EventHandler(this.B4_Click);
+            // 
+            // B5
+            // 
+            this.B5.BackColor = System.Drawing.Color.Black;
+            this.B5.Location = new System.Drawing.Point(237, 69);
+            this.B5.Name = "B5";
+            this.B5.Size = new System.Drawing.Size(50, 50);
+            this.B5.TabIndex = 5;
+            this.B5.UseVisualStyleBackColor = false;
+            this.B5.Click += new System.EventHandler(this.B5_Click);
+            // 
+            // C1
+            // 
+            this.C1.BackColor = System.Drawing.Color.Black;
+            this.C1.Location = new System.Drawing.Point(13, 126);
+            this.C1.Name = "C1";
+            this.C1.Size = new System.Drawing.Size(50, 50);
+            this.C1.TabIndex = 6;
+            this.C1.UseVisualStyleBackColor = false;
+            this.C1.Click += new System.EventHandler(this.C1_Click);
+            // 
+            // C2
+            // 
+            this.C2.BackColor = System.Drawing.Color.Black;
+            this.C2.Location = new System.Drawing.Point(69, 126);
+            this.C2.Name = "C2";
+            this.C2.Size = new System.Drawing.Size(50, 50);
+            this.C2.TabIndex = 7;
+            this.C2.UseVisualStyleBackColor = false;
+            this.C2.Click += new System.EventHandler(this.C2_Click);
+            // 
+            // C3
+            // 
+            this.C3.BackColor = System.Drawing.Color.Black;
+            this.C3.Location = new System.Drawing.Point(125, 126);
+            this.C3.Name = "C3";
+            this.C3.Size = new System.Drawing.Size(50, 50);
+            this.C3.TabIndex = 8;
+            this.C3.UseVisualStyleBackColor = false;
+            this.C3.Click += new System.EventHandler(this.C3_Click);
+            // 
+            // C4
+            // 
+            this.C4.BackColor = System.Drawing.Color.Black;
+            this.C4.Location = new System.Drawing.Point(181, 126);
+            this.C4.Name = "C4";
+            this.C4.Size = new System.Drawing.Size(50, 50);
+            this.C4.TabIndex = 9;
+            this.C4.UseVisualStyleBackColor = false;
+            this.C4.Click += new System.EventHandler(this.C4_Click);
+            // 
+            // C5
+            // 
+            this.C5.BackColor = System.Drawing.Color.Black;
+            this.C5.Location = new System.Drawing.Point(237, 126);
+            this.C5.Name = "C5";
+            this.C5.Size = new System.Drawing.Size(50, 50);
+            this.C5.TabIndex = 10;
+            this.C5.UseVisualStyleBackColor = false;
+            this.C5.Click += new System.EventHandler(this.C5_Click);
+            // 
+            // D1
+            // 
+            this.D1.BackColor = System.Drawing.Color.Black;
+            this.D1.Location = new System.Drawing.Point(13, 183);
+            this.D1.Name = "D1";
+            this.D1.Size = new System.Drawing.Size(50, 50);
+            this.D1.TabIndex = 11;
+            this.D1.UseVisualStyleBackColor = false;
+            this.D1.Click += new System.EventHandler(this.D1_Click);
+            // 
+            // D2
+            // 
+            this.D2.BackColor = System.Drawing.Color.Black;
+            this.D2.Location = new System.Drawing.Point(69, 183);
+            this.D2.Name = "D2";
+            this.D2.Size = new System.Drawing.Size(50, 50);
+            this.D2.TabIndex = 12;
+            this.D2.UseVisualStyleBackColor = false;
+            this.D2.Click += new System.EventHandler(this.D2_Click);
+            // 
+            // D3
+            // 
+            this.D3.BackColor = System.Drawing.Color.Black;
+            this.D3.Location = new System.Drawing.Point(125, 183);
+            this.D3.Name = "D3";
+            this.D3.Size = new System.Drawing.Size(50, 50);
+            this.D3.TabIndex = 13;
+            this.D3.UseVisualStyleBackColor = false;
+            this.D3.Click += new System.EventHandler(this.D3_Click);
+            // 
+            // D4
+            // 
+            this.D4.BackColor = System.Drawing.Color.Black;
+            this.D4.Location = new System.Drawing.Point(181, 183);
+            this.D4.Name = "D4";
+            this.D4.Size = new System.Drawing.Size(50, 50);
+            this.D4.TabIndex = 14;
+            this.D4.UseVisualStyleBackColor = false;
+            this.D4.Click += new System.EventHandler(this.D4_Click);
+            // 
+            // D5
+            // 
+            this.D5.BackColor = System.Drawing.Color.Black;
+            this.D5.Location = new System.Drawing.Point(237, 183);
+            this.D5.Name = "D5";
+            this.D5.Size = new System.Drawing.Size(50, 50);
+            this.D5.TabIndex = 15;
+            this.D5.UseVisualStyleBackColor = false;
+            this.D5.Click += new System.EventHandler(this.D5_Click);
+            // 
+            // E1
+            // 
+            this.E1.BackColor = System.Drawing.Color.Black;
+            this.E1.Location = new System.Drawing.Point(13, 240);
+            this.E1.Name = "E1";
+            this.E1.Size = new System.Drawing.Size(50, 50);
+            this.E1.TabIndex = 16;
+            this.E1.UseVisualStyleBackColor = false;
+            this.E1.Click += new System.EventHandler(this.E1_Click);
+            // 
+            // E2
+            // 
+            this.E2.BackColor = System.Drawing.Color.Black;
+            this.E2.Location = new System.Drawing.Point(69, 240);
+            this.E2.Name = "E2";
+            this.E2.Size = new System.Drawing.Size(50, 50);
+            this.E2.TabIndex = 17;
+            this.E2.UseVisualStyleBackColor = false;
+            this.E2.Click += new System.EventHandler(this.E2_Click);
+            // 
+            // E3
+            // 
+            this.E3.BackColor = System.Drawing.Color.Black;
+            this.E3.Location = new System.Drawing.Point(125, 240);
+            this.E3.Name = "E3";
+            this.E3.Size = new System.Drawing.Size(50, 50);
+            this.E3.TabIndex = 18;
+            this.E3.UseVisualStyleBackColor = false;
+            this.E3.Click += new System.EventHandler(this.E3_Click);
+            // 
+            // E4
+            // 
+            this.E4.BackColor = System.Drawing.Color.Black;
+            this.E4.Location = new System.Drawing.Point(181, 240);
+            this.E4.Name = "E4";
+            this.E4.Size = new System.Drawing.Size(50, 50);
+            this.E4.TabIndex = 19;
+            this.E4.UseVisualStyleBackColor = false;
+            this.E4.Click += new System.EventHandler(this.E4_Click);
+            // 
+            // E5
+            // 
+            this.E5.BackColor = System.Drawing.Color.Black;
+            this.E5.Location = new System.Drawing.Point(237, 240);
+            this.E5.Name = "E5";
+            this.E5.Size = new System.Drawing.Size(50, 50);
+            this.E5.TabIndex = 20;
+            this.E5.UseVisualStyleBackColor = false;
+            this.E5.Click += new System.EventHandler(this.E5_Click);
+            // 
+            // PatternSetup
+            // 
+            this.PatternSetup.AutoSize = true;
+            this.PatternSetup.BackColor = System.Drawing.Color.Transparent;
+            this.PatternSetup.ForeColor = System.Drawing.Color.Yellow;
+            this.PatternSetup.Location = new System.Drawing.Point(22, 317);
+            this.PatternSetup.Name = "PatternSetup";
+            this.PatternSetup.Size = new System.Drawing.Size(91, 17);
+            this.PatternSetup.TabIndex = 21;
+            this.PatternSetup.Text = "set up pattern";
+            this.PatternSetup.UseVisualStyleBackColor = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(303, 361);
+            this.Controls.Add(this.PatternSetup);
+            this.Controls.Add(this.E5);
+            this.Controls.Add(this.E4);
+            this.Controls.Add(this.E3);
+            this.Controls.Add(this.E2);
+            this.Controls.Add(this.E1);
+            this.Controls.Add(this.D5);
+            this.Controls.Add(this.D4);
+            this.Controls.Add(this.D3);
+            this.Controls.Add(this.D2);
+            this.Controls.Add(this.D1);
+            this.Controls.Add(this.C5);
+            this.Controls.Add(this.C4);
+            this.Controls.Add(this.C3);
+            this.Controls.Add(this.C2);
+            this.Controls.Add(this.C1);
+            this.Controls.Add(this.B5);
+            this.Controls.Add(this.B4);
+            this.Controls.Add(this.B3);
+            this.Controls.Add(this.B2);
+            this.Controls.Add(this.B1);
+            this.Controls.Add(this.A5);
+            this.Controls.Add(this.A4);
+            this.Controls.Add(this.A3);
+            this.Controls.Add(this.A2);
+            this.Controls.Add(this.Reset);
+            this.Controls.Add(this.A1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Lights Out";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button A1;
+        private System.Windows.Forms.Button A2;
+        private System.Windows.Forms.Button A3;
+        private System.Windows.Forms.Button A4;
+        private System.Windows.Forms.Button A5;
+        private System.Windows.Forms.Button Reset;
+        private System.Windows.Forms.Button B1;
+        private System.Windows.Forms.Button B2;
+        private System.Windows.Forms.Button B3;
+        private System.Windows.Forms.Button B4;
+        private System.Windows.Forms.Button B5;
+        private System.Windows.Forms.Button C1;
+        private System.Windows.Forms.Button C2;
+        private System.Windows.Forms.Button C3;
+        private System.Windows.Forms.Button C4;
+        private System.Windows.Forms.Button C5;
+        private System.Windows.Forms.Button D1;
+        private System.Windows.Forms.Button D2;
+        private System.Windows.Forms.Button D3;
+        private System.Windows.Forms.Button D4;
+        private System.Windows.Forms.Button D5;
+        private System.Windows.Forms.Button E1;
+        private System.Windows.Forms.Button E2;
+        private System.Windows.Forms.Button E3;
+        private System.Windows.Forms.Button E4;
+        private System.Windows.Forms.Button E5;
+        private System.Windows.Forms.CheckBox PatternSetup;
+    }
+}
+
